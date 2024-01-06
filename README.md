e8ed2c96a4ef86e6ec564c7c032d21e072cc53cf
Yann Klein <yann.klein@me.com>
2024-01-06 11:20:43 +0900

    build controller and view for restaurants

--- README.md
+++ README.md
@@ -3,22 +3,18 @@
 ## Data layer
 
 1. Create the emtpy DB (automatically by rails new)
-2. Create migration files/model
-3. Migrate
+2. Create migration files/model `rails g model Restaurant name rating:integer`
+3. Migrate `rails db:migrate`
 4. Validations
-5. Seed
+5. Seed `rails db:seed`
 
 ## R/C/V
 1. Routes
 2. Controller
 3. View
 
-### Create
-
-### Read 
-a. Read all
-b. Read one (details)
-
-### Udpate
-
-### Delete
+Create a restaurant
+See all restaurants
+See details of one restaurant
+Update a restaurant
+Delete a restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # verb 'path', to: 'controller#action', as: 'prefix'
+
+  # See all restaurants
+  get 'restaurants', to: 'restaurants#index', as: 'restaurants'
+  # # Create a restaurant
+  get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  # # See details of one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  # # Create a restaurant
+  post 'restaurants', to: 'restaurants#create'
+  # # Update a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  patch 'restaurants/:id', to: 'restaurants#update'
+  # # Delete a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants
+  # resources :restaurants, only: [:index, :show]
+  # resources :restaurants, except: [:index, :show]
+
 end
